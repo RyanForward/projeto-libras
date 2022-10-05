d8dd4f484ba00fd51fa094a6d12098520dc31d99
RyanEXT <83303260+RyanForward@users.noreply.github.com>
2022-10-05 20:11:22 -0300

    Update Letras.js

--- Letras-checks/Letras.js
+++ Letras-checks/Letras.js
@@ -1,16 +1,13 @@
 let score = 0
 let streak = 0
-
 function getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min)) + min;
       }
-
 function mostraScore(){
         alert(`O seu score atual é:  ${score}`)
 }
-
 function funcao(letra1, letra2){
         let letra = prompt ("Digite qual letra é essa: ")
         if (letra == letra1 || letra == letra2){
@@ -18,13 +15,13 @@ function funcao(letra1, letra2){
                 if(streak>2){
                     score += streak
                 }
-                score += 1
+                score += getRandomInt(1, 10)
                 alert(' Resposta certa! \n Seu score é: ' + score)
                 }
-    
+
         else{
                 streak = 0
-                score -= 1
+                score -= getRandomInt(2, 8)
                 alert("Resposta Errada! Essa é a letra " + letra1 + '\nSeu score é: ' + score)
                 }
 }
